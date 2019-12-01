6cfea329636448678006792defe1a11cfb52e00d
alegf <alegfonfach@gmail.com>
2019-12-01 19:20:28 -0400

    rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent, HomeComponent, ProductComponent } from './components/index.paginas';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'product', component: ProductComponent},
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,3 @@
+export { AboutComponent } from './about/about.component'
+export { HomeComponent } from './home/home.component'
+export { ProductComponent } from './product/product.component'
